bfc1c044120e88a2738b7b752edbb5768466f748
Adham-zakaria <es-adham.zakaria2027@alexu.edu.eg>
2026-09-25 19:15:09 +0300

    Stage 2: read endpoints with 404

--- server.js
+++ server.js
@@ -19,6 +19,26 @@ app.get("/health", (req, res) => {
     });
 });
 
+// list of tasks
+let tasks = [
+    { id: 1, title: 'Task 1', done: false },
+    { id: 2, title: 'Task 2', done: true },
+    { id: 3, title: 'Task 3', done: false }
+];
+
+app.get("/tasks", (req, res) => {
+    res.status(200).json(tasks);
+});
+
+app.get("/tasks/:id", (req, res) => {
+    const taskId = parseInt(req.params.id);
+    const task = tasks.find(t => t.id === taskId);
+    if (!task) {
+        return res.status(404).json({ error: `Task ${req.params.id} not found` });
+    }
+    res.status(200).json(task);
+});
+
 app.listen(PORT, () => {
     console.log(`Server is at http://localhost:${PORT}`);
 });
